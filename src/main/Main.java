08984747eb0c24d1f02ed3b8c23175db09e38035
Veteran <Veteran@192.168.200.138>
2018-01-31 22:52:17 -0500

    trying again

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,9 +1,35 @@
+/**
+ * John Locklear,Ben Tenhover, Kyle Warren
+ * Assignment02
+ * 01/31/2018
+ * IT2045C/02/Spring 2018
+ */
 package main;
-
+/**
+ * this is the main that will call and test the primeCheck class to see if it is true
+ * @author Veteran
+ *
+ */
+import java.util.Scanner;
 public class Main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args, boolean test01) {
+		// Declare and instantiate 
+		 System.out.println("Enter Number ");
+	        Scanner in = new Scanner( System.in );
+	        int num = in.nextInt();
+	        
+	        	
+	        if (test01 = true){
+				test01.isPrime;
+	        }
+	            System.out.println("it is prime");
+
+	        }
+	         else{
+	            System.out.println("it is not a prime");
+
+		
 
 	}
 
